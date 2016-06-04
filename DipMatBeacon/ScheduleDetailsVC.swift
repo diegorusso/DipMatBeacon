81b8b2b15506d89d095dfbdfbef198bd3100d23c
Diego Russo <me@diegor.it>
2016-06-04 12:35:11 +0100

    Implement share action in details view

--- DipMatBeacon/ScheduleDetailsVC.swift
+++ DipMatBeacon/ScheduleDetailsVC.swift
@@ -31,7 +31,6 @@ class ScheduleDetailsVC: UIViewController {
     @IBOutlet weak var bookingType: UILabel!
     @IBOutlet weak var lastChange: UILabel!
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
         setLabels()
@@ -40,6 +39,11 @@ class ScheduleDetailsVC: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(false)
         view.backgroundColor = correspondenceColor(schedule.correspondence)
+    }
+    
+    
+    @IBAction func shareSchedule(sender: UIBarButtonItem) {
+        shareScheduleDetails()
     }
 
 }
@@ -77,6 +81,39 @@ extension ScheduleDetailsVC {
         correspondence.text = schedule.correspondence
         bookingType.text = schedule.bookingType
         lastChange.text = completeDateFromDate(schedule.lastChange)
+    }
+    
+    func shareScheduleDetails(){
+        let activity1:String
+        let activity5:String
+        
+        if schedule.exam {
+            activity1 = "L'esame \"\(schedule.shortDescription)\""
+            activity5 = "Si prega di arrivare in orario altrimenti non si avrà diritto a sostenere l'esame."
+        } else {
+            activity1 = "La lezione \"\(schedule.shortDescription)\""
+            activity5 = ""
+        }
+        let activity2 = "del professore \(schedule.professor)"
+        let activity3 = "si svolgerà il giorno \(ISODateFromDate(schedule.startingTime)) dalle \(timeFromDate(schedule.startingTime)) alle \(timeFromDate(schedule.endTime))"
+        let activity4 = "in \"\(schedule.location.name)\" (piano \(schedule.location.floor) di \(schedule.location.building))."
+        let activity6 = "L'utilizzo di dispositivi mobili non è ammesso."
+        
+        let activity7 = "\n\n(Condiviso automaticamente tramite l'app DipMatBeacon)"
+        
+        let activityViewController: UIActivityViewController = UIActivityViewController(activityItems: [activity1, activity2, activity3, activity4, activity5, activity6, activity7], applicationActivities: nil)
+        
+        // To disable email sharing
+        //activityViewController.excludedActivityTypes = [UIActivityTypeMail]
+        
+        activityViewController.completionWithItemsHandler = {
+            (activity, success, items, error) in
+            
+            if activity == UIActivityTypeMail {
+                print("Email selected")
+            }
+        }
+        self.presentViewController(activityViewController, animated: true, completion: nil)
     }
 
 }
